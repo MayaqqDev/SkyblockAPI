b380b6937b0478822b39e438fbc406713b3a3321
Gravy Boat <gravy@thatgravyboat.tech>
2024-12-09 17:12:13 -0330

    Add uuid and enchantments data type

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/datatype/defaults/GenericDataTypes.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/datatype/defaults/GenericDataTypes.kt
@@ -1,6 +1,7 @@
 package tech.thatgravyboat.skyblockapi.api.datatype.defaults
 
 import kotlinx.datetime.Instant
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.nbt.NumericTag
 import net.minecraft.nbt.Tag
 import tech.thatgravyboat.skyblockapi.api.datatype.DataType
@@ -8,30 +9,38 @@ import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.misc.RegisterDataTypesEvent
 import tech.thatgravyboat.skyblockapi.modules.Module
 import tech.thatgravyboat.skyblockapi.utils.extentions.getTag
+import java.util.UUID
 
 @Module
 object GenericDataTypes {
 
     val ID: DataType<String> = DataType("id") { it.getTag("id")?.asString }
+    val UUID: DataType<UUID> = DataType("uuid") { runCatching { it.getTag("uuid")?.asString?.let(java.util.UUID::fromString) }.getOrNull() }
     val MODIFIER: DataType<String> = DataType("modifier") { it.getTag("modifier")?.asString }
     val TIMESTAMP: DataType<Instant> = DataType("timestamp") { it.getTag("timestamp")?.asLong?.let(Instant::fromEpochMilliseconds) }
     val SECONDS_HELD: DataType<Int> = DataType("seconds_held") { it.getTag("seconds_held")?.asInt }
     val PICKONIMBUS_DURABILITY: DataType<Int> = DataType("pickonimbus_durability") { it.getTag("pickonimbus_durability")?.asInt }
     val RARITY_UPGRADES: DataType<Int> = DataType("rarity_upgrades") { it.getTag("rarity_upgrades")?.asInt }
     val QUIVER_ARROW: DataType<Boolean> = DataType("quiver_arrow") { it.getTag("quiver_arrow")?.asString?.equals("true") }
+    val ENCHANTMENTS: DataType<Map<String, Int>> = DataType("enchantments") { it.getTag("enchantment")?.asObject?.let { tag ->
+        buildMap { tag.allKeys.forEach { key -> this[key] = tag.getInt(key) } } }
+    }
 
     @Subscription
     fun onDataTypeRegistration(event: RegisterDataTypesEvent) {
         event.register(ID)
+        event.register(UUID)
         event.register(MODIFIER)
         event.register(TIMESTAMP)
         event.register(SECONDS_HELD)
         event.register(PICKONIMBUS_DURABILITY)
         event.register(RARITY_UPGRADES)
         event.register(QUIVER_ARROW)
+        event.register(ENCHANTMENTS)
     }
 
     private val Tag.asInt get() = (this as? NumericTag)?.asInt
     private val Tag.asLong get() = (this as? NumericTag)?.asLong
+    private val Tag.asObject get() = (this as? CompoundTag)
 
 }
